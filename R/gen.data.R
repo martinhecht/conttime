2da59c47932ba91c26330f73db3265f55bb317c4
martinhecht <martin.hecht@hsu-hh.de>
2024-04-10 00:30:24 +0200

    MH 2024-04-10 v0.0.1

---
+++ R/gen.data.R
@@ -0,0 +1,202 @@
+## Changelog:
+# MH 2024-04-04: set up
+
+## Documentation
+#' @title
+#' @description
+#' @param
+#' @param
+#' @param
+#' @return
+
+## Function definition
+gen.data <- function( defaults=c(1), env=NULL, seed="random", verbose=FALSE ){
+		
+		### based on tvct_v1.pdf (2024-04-04)
+
+		# require packages
+		require( mvtnorm )
+		require( expm )
+	
+		# seed
+		if( seed %in% "random" ){
+			seed <- sample( 1:999999999, 1 )
+		}
+		set.seed( seed )
+		
+		# get empty structures
+		if( is.null( env ) ) env <- gen.empty.structures()
+
+		# put all elements from environment here
+		names <- ls(envir=env)
+		for( i in 1:length( names ) ){
+			eval( parse( text=paste0( names[i], " <- get('", names[i], "', envir=env)" ) ) )
+		}
+
+		## gen design characteristics
+		# Tj
+		Tj <- as.integer( sample( as.character(min(2,T):T), N, replace = TRUE ) )
+		# deltajp
+		for( j in 1:N ){
+			for( p in 1:(Tj[j]-1) ){
+				deltajp[j,p] <- sample( c(0.5,1,1.5), 1 )
+			}
+		}
+		# tjp
+		for( j in 1:N ){
+			# first time point
+			tjp[j,1] <- sample( c(-1,-0.5,0,0.5,1), 1 )
+			for( p in 2:Tj[j] ){
+				tjp[j,p] <- tjp[j,p-1] + deltajp[j,p-1]
+			}
+		}
+		
+		A0[] <- 0.1
+		diag( A0 ) <- -0.75
+		Q0[] <- 0.25
+		diag( Q0 ) <- 1
+		mu0[] <- 0
+		
+		if( defaults %in% 1 ){
+			Achange[] <- 0
+			diag( Achange ) <- 0
+			Qchange[] <- 0
+			diag( Qchange ) <- 0
+			muchange[]  <- 0
+		}
+		
+		# Achange[1,1] <- c(0 , 0.020 , 0.030 , 0.040 )
+		# Achange[2,2] <- c(0 , 0.020 , 0.030 , 0.040 )
+		# Achange[1,2] <- c(0 , 0.010 , 0.015 , 0.020 )
+		# Achange[2,1] <- c(0 , 0.010 , 0.015 , 0.020 )
+		# Achange <- -Achange*0.9
+
+		# Qchange[1,1] <- c(0 , 0.025 , 0.050 , 0.075 )
+		# Qchange[2,2] <- c(0 , 0.025 , 0.050 , 0.075 )
+		# Qchange[1,2] <- c(0 , 0.010 , 0.020 , 0.030 )
+		# Qchange[2,1] <- c(0 , 0.010 , 0.020 , 0.030 )
+		# Qchange <- -Qchange
+
+		# muchange[1] <- c(0 , 0.1 , 0.2 , 0.3 )
+		# muchange[2] <- c(0 , 0.1 , 0.2 , 0.3 )
+		# muchange <- muchange*0.5
+
+		Sigmamu[] <- 0
+		diag( Sigmamu ) <- 1
+
+		# error covariance matrices
+		SigmaepsA[] <- 0
+		diag( SigmaepsA ) <- 0.0025
+		SigmaepsQ[] <- 0
+		diag( SigmaepsQ ) <- 0.0025
+		Sigmaepsmu[] <- 0
+		diag( Sigmaepsmu ) <- 0.0025
+
+		# muj, Eq. 8
+		for( j in 1:N ){
+			muj[,1,j] <- rmvnorm( 1, mean=zerovecF, sigma=Sigmamu )
+		}
+		
+		for( j in 1:N ){
+			for( p in 1:Tj[j] ){
+				# Eq. 2
+				epsAjp[,,j,p] <- rmvnorm( 1, mean=zerovecF2, sigma=SigmaepsA )
+				Ajp[,,j,p] <- irow( S1 %*% row( A0 + Achange*tjp[j,p] + irow(epsAjp[,,j,p,drop=FALSE]) ) ) + irow( S2 %*% row( A0 * exp( -(Achange*tjp[j,p] + irow(epsAjp[,,j,p,drop=FALSE]) ) ) ) )
+				# Eq. 3
+				epsQjp[,,j,p] <- rmvnorm( 1, mean=zerovecFF12, sigma=SigmaepsQ )
+				Qjp[,,j,p] <- irow( S1 %*% row( Q0 + Qchange*tjp[j,p] + irow(S3%*%epsQjp[,,j,p,drop=FALSE]) ) ) + irow( S2 %*% row( Q0 * exp( Qchange*tjp[j,p] + irow(S3%*%epsQjp[,,j,p,drop=FALSE]) ) ) )
+				# Eq. 10
+				epsmujp[,1,j,p] <- rmvnorm( 1, mean=zerovecF, sigma=Sigmaepsmu )
+				# Eq. 9
+				mujp[,1,j,p] <- mu0 + muchange*tjp[j,p] + as.matrix( epsmujp[,1,j,p,drop=FALSE] ) + as.matrix( muj[,1,j,drop=FALSE] )
+				# Ahash, Eq. 14
+				Ahashjp[,,j,p] <- Ajp[,,j,p] %x% IF + IF %x% Ajp[,,j,p]
+				# Sigmaw, Eq. 14
+				Sigmawjp[,,j,p] <- irow( -solve( Ahashjp[,,j,p] ) %*% row(Qjp[,,j,p]) )
+			}
+		}
+
+		for( j in 1:N ){
+			for( p in 1:(Tj[j]-1) ){
+				# Astarjp, Eq. 12
+				Astarjp[,,j,p] <- expm( Ajp[,,j,p] * deltajp[j,p] )
+				# Qstarjp, Eq. 13
+				Qstarjp[,,j,p] <- irow( -( expm( Ahashjp[,,j,p] * deltajp[j,p] ) - IF2 ) %*% row( Sigmawjp[,,j,p] ) )			
+			}
+		}
+
+		for( j in 1:N ){
+			# theta, first time point, Eq. 17
+			thetajp[,1,j,1] <- rmvnorm( 1, mean=zerovecF, sigma=Sigmawjp[,,j,1] )
+			for( p in 2:Tj[j] ){
+				# omegajp, Eq. 15
+				omegajp[,1,j,p] <- rmvnorm( 1, mean=zerovecF, sigma=Qstarjp[,,j,p-1] )
+				# thetajp, Eq. 16
+				thetajp[,1,j,p] <- Astarjp[,,j,p-1] %*% as.matrix( thetajp[,1,j,p-1,drop=FALSE] ) + ( IF - Astarjp[,,j,p-1] ) %*% as.matrix( mujp[,1,j,p,drop=FALSE] ) + as.matrix( omegajp[,1,j,p,drop=FALSE] )
+			}
+		}
+	
+		## Delta (default)
+		nitems.per.dim <- floor( I/F )
+		Delta[] <- 0
+		for( f in 1:F ){
+			Delta[ ((f-1)*nitems.per.dim+1):(f*nitems.per.dim) ,f] <- 1
+		}
+		if( any( rs <- rowSums( Delta ) == 0 ) ){
+			Delta[which(rs),F] <- 1
+		}
+
+		Sigmaeps[] <- 0
+		diag( Sigmaeps ) <- 0.10
+		
+		for( j in 1:N ){
+			for( p in 1:Tj[j] ){
+				# measurement error, Eq. 19
+				epsjp[,1,j,p] <- rmvnorm( 1, mean=zerovecI, sigma=Sigmaeps )
+				# responses, Eq. 18
+				yjp[,1,j,p] <- Delta %*% as.matrix( thetajp[,1,j,p,drop=FALSE] ) + as.matrix( epsjp[,1,j,p,drop=FALSE] )
+			}
+		}
+		
+		env2 <- new.env()
+		for( i in 1:length( str.names ) ){
+			assign( str.names[i], eval( parse( text=str.names[i] ) ), envir = env2, inherits = FALSE, immediate=TRUE )
+		}
+		return( env2 )
+}
+
+
+### development
+# user.profile <- shell( "echo %USERPROFILE%", intern=TRUE )
+# Rfiles.folder <- file.path( user.profile,
+                                    # "Dropbox/139_conttime/conttime/R" )
+# Rfiles <- list.files( Rfiles.folder , pattern="*.R" )
+# Rfiles <- Rfiles[ !Rfiles %in% c("gen.data.R") ]
+# for( Rfile in Rfiles ){
+	# source( file.path( Rfiles.folder, Rfile ) )
+# }
+
+# m <- gen.data()
+# ls( envir=m )
+
+# while( TRUE ){
+	# e <- gen.empty.structures()
+	# e <- gen.empty.structures(T=5,N=8,F=2,I=11)
+	# m <- gen.data(env=e)
+	# print( get( "Tj", envir=m, inherits=FALSE ) ); flush.console()
+# }
+
+# get( "Tj", envir=m, inherits=FALSE )
+# get( "yjp", envir=m, inherits=FALSE )
+# get( "Delta", envir=m, inherits=FALSE )
+# get( "Sigmaeps", envir=m, inherits=FALSE )
+# get( "A0", envir=m, inherits=FALSE )
+# get( "Achange", envir=m, inherits=FALSE )
+# get( "Tj", envir=m, inherits=FALSE )
+# get( "deltajp", envir=m, inherits=FALSE )
+
+
+
+### test
+# require( testthat )
+# test_file("../tests/testthat/XXXXX.R")
---
+++ R/gen.empty.structures.R
@@ -0,0 +1,154 @@
+## Changelog:
+# MH 2024-04-04: set up
+
+## Documentation
+#' @title
+#' @description
+#' @param
+#' @param
+#' @param
+#' @return
+
+## Function definition
+gen.empty.structures <- function( F=2, I=F, N=5, T=3, env=NULL, verbose=FALSE ){
+		
+		### based on tvct_v1.pdf (2024-04-04)
+		
+		## F x F structures
+		FxF.names <- c( "A0","Achange","Q0","Qchange","Sigmamu","Sigmaepsmu" )
+		eval( parse( text = paste0( FxF.names, " <- array( as.numeric(NA), dim=c(F,F) )" ) ) )
+		# identity matrix
+		IF <- diag( F )
+		FxF.names <- c( FxF.names, "IF" )
+		
+		## I x F structures
+		IxF.names <- c( "Lambda" )
+		eval( parse( text = paste0( IxF.names, " <- array( as.numeric(NA), dim=c(I,F) )" ) ) )
+		
+		## I x I structures
+		IxI.names <- c( "Sigmaeps" )
+		eval( parse( text = paste0( IxI.names, " <- array( as.numeric(NA), dim=c(I,I) )" ) ) )
+
+		## I x 1 structures
+		Ix1.names <- c( "zerovecI" )
+		eval( parse( text = paste0( Ix1.names, " <- array( as.numeric(0), dim=c(I) )" ) ) )
+		
+		## F x 1 structures
+		Fx1.names1 <- c( "mu0","muchange" )
+		eval( parse( text = paste0( Fx1.names1, " <- array( as.numeric(NA), dim=c(F,1) )" ) ) )
+		Fx1.names2 <- c( "zerovecF" )
+		eval( parse( text = paste0( Fx1.names2, " <- array( as.numeric(0), dim=c(F,1) )" ) ) )
+		Fx1.names <- c(Fx1.names1,Fx1.names2)
+
+		## F x 1 x N structures
+		Fx1xN.names <- c( "muj" )
+		eval( parse( text = paste0( Fx1xN.names, " <- array( as.numeric(NA), dim=c(F,1,N) )" ) ) )
+		
+		## F x 1 x N x T structures
+		Fx1xNxT.names <- c( "thetajp","omegajp","mujp", "epsmujp" )
+		eval( parse( text = paste0( Fx1xNxT.names, " <- array( as.numeric(NA), dim=c(F,1,N,T) )" ) ) )		
+
+		## I x 1 x N x T structures
+		Ix1xNxT.names <- c( "yjp","epsjp" )
+		eval( parse( text = paste0( Ix1xNxT.names, " <- array( as.numeric(NA), dim=c(I,1,N,T) )" ) ) )		
+		
+		## F x F x N x T
+		FxFxNxT.names <- c( "Ajp","Astarjp","Qjp","Qstarjp","Sigmawjp" )
+		eval( parse( text = paste0( FxFxNxT.names, " <- array( as.numeric(NA), dim=c(F,F,N,T) )" ) ) )
+
+		## F^2 x 1 x N x T
+		F2x1xNxT.names <- c( "epsAjp" )
+		eval( parse( text = paste0( F2x1xNxT.names, " <- array( as.numeric(NA), dim=c(F^2,1,N,T) )" ) ) )
+
+		## F(F+1)/2 x 1 x N x T
+		FF12x1xNxT.names <- c( "epsQjp" )
+		eval( parse( text = paste0( FF12x1xNxT.names, " <- array( as.numeric(NA), dim=c(F*(F+1)/2,1,N,T) )" ) ) )
+		
+		## F^2 x F^2 x N x T
+		F2xF2xNxT.names <- c( "Ahashjp" )
+		eval( parse( text = paste0( F2xF2xNxT.names, " <- array( as.numeric(NA), dim=c(F^2,F^2,N,T) )" ) ) )
+		
+		## N x T structures
+		NxT.names <- c( "tjp" )
+		eval( parse( text = paste0( NxT.names, " <- array( as.numeric(NA), dim=c(N,T) )" ) ) )
+
+		## N x (T-1) structures
+		NxTmin1.names <- c( "deltajp" )
+		eval( parse( text = paste0( NxTmin1.names, " <- array( as.numeric(NA), dim=c(N,T-1) )" ) ) )
+		
+		## F^2 x F^2 structures
+		F2xF2.names <- c( "SigmaepsA" )
+		eval( parse( text = paste0( F2xF2.names, " <- array( as.numeric(NA), dim=c(F^2,F^2) )" ) ) )
+		# Selection matrices
+		S2 <- array( 0, dim=c(F^2,F^2) )
+		f2 <- function( n ) n*(F+1)-F
+		for( n in 1:F ){
+			S2[f2(n),f2(n)] <- 1
+		}
+		S1 <- array( 0, dim=c(F^2,F^2) )
+		diag( S1 ) <- -(diag(S2) - 1)
+		# identity matrix
+		IF2 <- diag( F^2 )
+		F2xF2.names <- c( F2xF2.names, "S1","S2","IF2" )
+
+		## F^2 x 1 structures
+		F2x1.names <- c( "zerovecF2" )
+		eval( parse( text = paste0( F2x1.names, " <- array( as.numeric(0), dim=c(F^2,1) )" ) ) )
+		
+		## F(F+1)/2 x F(F+1)/2 structures
+		FF12xFF12.names <- c( "SigmaepsQ" )
+		eval( parse( text = paste0( FF12xFF12.names, " <- array( as.numeric(NA), dim=c(F*(F+1)/2,F*(F+1)/2) )" ) ) )
+		
+		## F(F+1)/2 x 1 structures
+		FF12x1.names <- c( "zerovecFF12" )
+		eval( parse( text = paste0( FF12x1.names, " <- array( as.numeric(0), dim=c(F*(F+1)/2,1) )" ) ) )
+		
+		## N structures
+		N.names <- c( "Tj" )
+		eval( parse( text = paste0( N.names, " <- array( as.numeric(NA), dim=c(N) )" ) ) )
+		
+		## I x F structures
+		IxF.names <- c( "Delta" )
+		eval( parse( text = paste0( IxF.names, " <- array( as.numeric(NA), dim=c(I,F) )" ) ) )
+		
+		## special structures
+		S3 <- NULL
+		# S3 for F=2
+		if( F==2 ) S3 <- matrix( c(1,0,0,0,  0,1,1,0,  0,0,0,1), nrow=F^2, ncol=F*(F+1)/2 )
+		special.names <- "S3"
+		
+		# environment
+		if( is.null( env ) ){
+			env <- new.env()
+		}
+		str.names <- c( FxF.names, IxF.names, IxI.names, Ix1.names, Fx1.names, Fx1xN.names, Fx1xNxT.names, Ix1xNxT.names, FxFxNxT.names, F2x1xNxT.names, FF12x1xNxT.names, F2xF2xNxT.names, NxT.names, NxTmin1.names, F2xF2.names, F2x1.names, FF12xFF12.names, FF12x1.names, N.names, IxF.names, special.names, "F", "I", "N", "T", "str.names" )
+		for( i in 1:length( str.names ) ){
+			assign( str.names[i], eval( parse( text=str.names[i] ) ), envir = env, inherits = FALSE, immediate=TRUE )
+		}
+		
+		return( env )
+}
+
+
+### development
+# user.profile <- shell( "echo %USERPROFILE%", intern=TRUE )
+# Rfiles.folder <- file.path( user.profile,
+                                    # "Dropbox/139_conttime/conttime/R" )
+# Rfiles <- list.files( Rfiles.folder , pattern="*.R" )
+# Rfiles <- Rfiles[ !Rfiles %in% c("gen.empty.structures.R") ]
+# for( Rfile in Rfiles ){
+	# source( file.path( Rfiles.folder, Rfile ) )
+# }
+
+# print( m <- gen.empty.structures() )
+# ls( envir=m )
+# get( "A0", envir=m, inherits=FALSE )
+# get( "Achange", envir=m, inherits=FALSE )
+# get( "Tj", envir=m, inherits=FALSE )
+# get( "deltajp", envir=m, inherits=FALSE )
+
+
+
+### test
+# require( testthat )
+# test_file("../tests/testthat/XXXXX.R")
